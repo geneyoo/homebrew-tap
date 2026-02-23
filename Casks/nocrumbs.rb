815df413f31364985ea80917e652d61d742012e1
Gene <geneyoois@gmail.com>
2026-02-22 22:44:24 -0500

    chore: bump nocrumbs cask to v0.4.2

---
+++ Casks/nocrumbs.rb
@@ -0,0 +1,18 @@
+cask "nocrumbs" do
+  version "0.4.2"
+  sha256 "5bba198a7b9f03495d509de0930387f5233ec29a67aae8c38175ac1ec4457bbb"
+
+  url "https://github.com/geneyoo/nocrumbs/releases/download/v#{version}/NoCrumbs-#{version}.zip"
+  name "NoCrumbs"
+  desc "Catch every crumb your agent leaves behind"
+  homepage "https://nocrumbs.ai"
+
+  depends_on macos: ">= :sonoma"
+
+  app "NoCrumbs.app"
+  binary "#{appdir}/NoCrumbs.app/Contents/Resources/nocrumbs"
+
+  zap trash: [
+    "~/Library/Application Support/NoCrumbs",
+  ]
+end
